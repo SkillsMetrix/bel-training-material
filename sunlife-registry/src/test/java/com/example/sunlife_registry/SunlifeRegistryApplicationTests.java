68f6e84d8e2fb6abfc3c14d02d3e92638ac989e4
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2026-09-10 08:40:51 +0530

    Add files via upload

---
+++ sunlife-registry/src/test/java/com/example/sunlife_registry/SunlifeRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sunlife_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunlifeRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
