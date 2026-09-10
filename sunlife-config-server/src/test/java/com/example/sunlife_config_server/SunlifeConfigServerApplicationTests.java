89c61629eb4372695a48a39cfc5d20b4dc2b42fb
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2026-09-10 08:38:41 +0530

    Add files via upload

---
+++ sunlife-config-server/src/test/java/com/example/sunlife_config_server/SunlifeConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sunlife_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunlifeConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
